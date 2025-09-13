dae1cbc1bf89c418e5c21aa6e83ddf7265e18d61
priyadharshan1312 <dharshanmohan13@gmail.com>
2025-09-13 12:00:47 +0530

    Create if,else if,else.cpp

---
+++ if,else if,else.cpp
@@ -0,0 +1,12 @@
+int main(){
+	int age =8;
+	if(age>=18){
+		printf("you can vote");
+	}else if(age>=5){
+		printf("go to school");
+	}else if(age>=3){
+		printf("you go to kinder garden");
+	}else{
+		printf("you are baby");
+	}
+	}
