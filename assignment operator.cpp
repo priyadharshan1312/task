1aad88c142c4fddfa1c37eaf2e0294e1327efc1f
priyadharshan1312 <dharshanmohan13@gmail.com>
2025-09-13 11:59:05 +0530

    Create assignment operator.cpp

---
+++ assignment operator.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+	int i = 1000;
+	printf("%d\n",i);
+	i+=2000;
+	printf("%d\n",i);
+	i-=1500;
+	printf("%d\n",i);
+	i*=5000;
+	printf("%d\n",i);
+	i/=1000;
+	printf("%d\n",i);
+	i%=2000;
+	printf("%d",i);
+	return 0;
+}
